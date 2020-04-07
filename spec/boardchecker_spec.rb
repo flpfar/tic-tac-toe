f52972edeb482465b81b3b55a6e307de1230d325
Felipe Rosa <flp.far@gmail.com>
2020-04-07 17:54:47 -0300

    Refactor boardchecker_spec to separate it blocks

--- spec/boardchecker_spec.rb
+++ spec/boardchecker_spec.rb
@@ -1,33 +1,55 @@
 require_relative '../lib/boardchecker'
 
 describe BoardChecker do
-  it 'finds a winner in horizontal positions (Game positions: 123, 456, 789)' do
-    case123 = ['x', 'x', 'x', 4, 'o', 'o', 7, 8, 9]
-    case456 = [1, 'o', 'o', 'x', 'x', 'x', 7, 8, 9]
-    case789 = [1, 'o', 'o', 4, 5, 6, 'x', 'x', 'x']
-    expect(BoardChecker.winner?(case123)).to be true
-    expect(BoardChecker.winner?(case456)).to be true
-    expect(BoardChecker.winner?(case789)).to be true
+  let(:test_board) { [] }
+  context 'when there is a winner in horizontal positions' do
+    it 'finds a winner in game positions 1 2 3' do
+      test_board = ['x', 'x', 'x', 4, 'o', 'o', 7, 8, 9]
+      expect(BoardChecker.winner?(test_board)).to be true
+    end
+
+    it 'finds a winner in game positions 4 5 6' do
+      test_board = [1, 'o', 'o', 'x', 'x', 'x', 7, 8, 9]
+      expect(BoardChecker.winner?(test_board)).to be true
+    end
+
+    it 'finds a winner in game positions 7 8 9' do
+      test_board = [1, 'o', 'o', 4, 5, 6, 'x', 'x', 'x']
+      expect(BoardChecker.winner?(test_board)).to be true
+    end
   end
 
-  it 'finds a winner in vertical positions (Game positions: 147, 258, 369)' do
-    case147 = ['x', 2, 3, 'x', 'o', 'o', 'x', 8, 9]
-    case258 = [1, 'o', 'x', 'x', 'o', 'x', 7, 'o', 9]
-    case369 = [1, 'o', 'x', 4, 5, 'x', 7, 'o', 'x']
-    expect(BoardChecker.winner?(case147)).to be true
-    expect(BoardChecker.winner?(case258)).to be true
-    expect(BoardChecker.winner?(case369)).to be true
+  context 'when there is a winner in vertical positions' do
+    it 'finds a winner in game positions 1 4 7' do
+      test_board = ['x', 2, 3, 'x', 'o', 'o', 'x', 8, 9]
+      expect(BoardChecker.winner?(test_board)).to be true
+    end
+
+    it 'finds a winner in game positions 2 5 8' do
+      test_board = [1, 'o', 'x', 'x', 'o', 'x', 7, 'o', 9]
+      expect(BoardChecker.winner?(test_board)).to be true
+    end
+
+    it 'finds a winner in game positions 3 6 9' do
+      test_board = [1, 'o', 'x', 4, 5, 'x', 7, 'o', 'x']
+      expect(BoardChecker.winner?(test_board)).to be true
+    end
   end
 
-  it 'finds a winner in diagonal positions (Game positions: 159, 357)' do
-    case159 = ['o', 'x', 'x', 4, 'o', 6, 'x', 8, 'o']
-    case357 = ['o', 2, 'x', 4, 'x', 6, 'x', 8, 'o']
-    expect(BoardChecker.winner?(case159)).to eq(true)
-    expect(BoardChecker.winner?(case357)).to eq(true)
+  context 'when there is a winner in diagonal positions' do
+    it 'finds a winner in game positions 1 5 9' do
+      test_board = ['o', 'x', 'x', 4, 'o', 6, 'x', 8, 'o']
+      expect(BoardChecker.winner?(test_board)).to be true
+    end
+
+    it 'finds a winner in game positions 3 5 7' do
+      test_board = ['o', 2, 'x', 4, 'x', 6, 'x', 8, 'o']
+      expect(BoardChecker.winner?(test_board)).to be true
+    end
   end
 
   it 'finds no winner' do
-    board = [1, 'o', 3, 'x', 5, 6, 'x', 8, 9]
-    expect(BoardChecker.winner?(board)).to eq(false)
+    test_board = [1, 'o', 3, 'x', 5, 6, 'x', 8, 9]
+    expect(BoardChecker.winner?(test_board)).to eq(false)
   end
 end
